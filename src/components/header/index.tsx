066b8f15d4162a0cdc1b3a55e670e60c0482c924
m00k <christian.bican@gmail.com>
2021-04-25 09:39:49 +0200

    remove router, unused pages

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,4 @@
 import { FunctionalComponent, h } from 'preact';
-import { Link } from 'preact-router/match';
 import style from './style.css';
 
 const Header: FunctionalComponent = () => {
@@ -7,15 +6,11 @@ const Header: FunctionalComponent = () => {
         <header class={style.header}>
             <h1>Preact App</h1>
             <nav>
-                <Link activeClassName={style.active} href="/">
-                    Home
-                </Link>
-                <Link activeClassName={style.active} href="/profile">
-                    Me
-                </Link>
-                <Link activeClassName={style.active} href="/profile/john">
-                    John
-                </Link>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
